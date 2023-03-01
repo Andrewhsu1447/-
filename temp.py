2e70de802343ba1fefa2d60c9a8b940b06d23a1e
syuan1447 <syuan1447@gmail.com>
2023-03-01 10:32:54 +0800

    字串合併與數字加減(Python)

--- temp.py
+++ temp.py
@@ -4,9 +4,12 @@ Spyder Editor
 
 This is a temporary script file.
 """
-
-x=10;y=20;z=30;
-sum = x+\
-      y+\
-      z    
-print(sum)      
+num1 = 43
+num2 = 4
+num3 = 3
+string1 = 'The'
+string2 = 'Completion Of This Task is Worth'
+string3 = 'points towards my midterm grade'
+string4 = ' '
+str(int((num1 - num3) / num2))
+string1+string4+string2.lower()+string4+str(int((num1 - num3) / num2))+string4+string3
